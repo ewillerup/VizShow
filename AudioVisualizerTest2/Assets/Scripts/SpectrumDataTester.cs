ddd0629d63f6c1f88e8c385c99db0b6fddfaedfc
devzorsmallz <46795817+devzorsmallz@users.noreply.github.com>
2021-02-19 13:29:50 -0700

    unity files

---
+++ AudioVisualizerTest2/Assets/Scripts/SpectrumDataTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectrumDataTester : MonoBehaviour
+{
+    public static float[] samples = new float[512];
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
