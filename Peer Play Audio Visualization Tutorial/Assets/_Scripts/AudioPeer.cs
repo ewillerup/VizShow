32ec2d65b2a773f799c833f87269c1c7e5438218
devzorsmallz <46795817+devzorsmallz@users.noreply.github.com>
2021-03-04 19:42:46 -0700

    Added New Project

---
+++ Peer Play Audio Visualization Tutorial/Assets/_Scripts/AudioPeer.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    float[] _bufferDecrease = new float[8];
+
+    float[] _freqBandHighest = new float[8];
+    public static float[] _audioBand = new float[8];
+    public static float[] _audioBandBuffer = new float[8];
+
+    public static float _amplitude, _amplitudeBuffer;
+    float _amplitudeHighest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+        GetAmplitude();
+    }
+
+    void GetAmplitude()
+    {
+        float _currentAmplitude = 0;
+        float _currentAmplitudeBuffer = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            _currentAmplitude += _audioBand[i];
+            _currentAmplitudeBuffer += _audioBandBuffer[i];
+        }
+
+        if (_currentAmplitude > _amplitudeHighest)
+        {
+            _amplitudeHighest = _currentAmplitude;
+        }
+
+        _amplitude = _currentAmplitude / _amplitudeHighest;
+        _amplitudeBuffer = _currentAmplitudeBuffer / _amplitudeHighest;
+    }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (_freqBand[i] > _freqBandHighest[i])
+            {
+                _freqBandHighest[i] = _freqBand[i];
+            }
+
+            _audioBand[i] = (_freqBand[i] / _freqBandHighest[i]);
+            _audioBandBuffer[i] = (_bandBuffer[i] / _freqBandHighest[i]);
+        }
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; g++)
+        {
+            if (_freqBand[g] > _bandBuffer[g])
+            {
+                _bandBuffer[g] = _freqBand[g];
+                _bufferDecrease[g] = 0.0005f;
+            }
+
+            if (_freqBand[g] < _bandBuffer[g])
+            {
+                _bandBuffer[g] -= _bufferDecrease[g];
+                _bufferDecrease[g] *= 1.2f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+        /*
+         * 22050 / 512 = ~43hz per sample
+         * 
+         * 20 - 60hz
+         * 60 - 250hz
+         * 250 - 500hz
+         * 500 - 2000hz
+         * 2000 - 4000hz
+         * 4000 - 6000hz
+         * 6000 - 2000hz
+         * 
+         * 0: 2 samples, 86hz
+         * 1: 4 samples, 172hz, 87 - 258hz
+         * 2: 8 samples, 344hz, 259 - 602hz
+         * 3: 16 samples, 688hz, 603 - 1290hz
+         * 4: 32 samples, 1376hz, 1291 - 2666hz
+         * 5: 64 samples, 2752hz, 2667 - 5418hz
+         * 6: 128 samples, 5504hz, 5419 - 10922hz
+         * 7: 256 samples, 110008hz, 10923 - 21930hz
+         * = 510 samples
+         */
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2; // makes up for the missing 2 samples
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            _freqBand[i] = average * 10;
+        }
+    }
+}
