880f9ed5a7ce716e628b097efe7011518547e4e9
devzorsmallz <46795817+devzorsmallz@users.noreply.github.com>
2021-03-05 12:34:30 -0700

    Adding Device Picker (In Progress)

--- Audiovisualizer/Assets/_Scripts/MicrophoneAudio.cs
+++ Audiovisualizer/Assets/_Scripts/MicrophoneAudio.cs
@@ -4,50 +4,71 @@ using UnityEngine;
 
 public class MicrophoneAudio : MonoBehaviour
 {
+    public int chosenDevice;
     public float frequency;
-    public int audioSampleRate = 44100;
     public string microphone;
     public FFTWindow fftWindow;
 
-    private int samples = 8192;
+    private int count;
+    private bool inputDeviceExists = false;
     private AudioSource audioSource;
+    private List<string> options = new List<string>();
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
 
-        foreach (string device in Microphone.devices)
+        if (Microphone.devices.Length > 0)
         {
-            if (microphone == null)
+            foreach (string device in Microphone.devices)
             {
-                microphone = device;
-            }
-        }
+                if (microphone == null)
+                {
+                    microphone = device;
+                }
 
-        UpdateMicrophone();
-    }
+                count++;
 
-    void UpdateMicrophone()
-    {
-        audioSource.Stop();
-        audioSource.clip = Microphone.Start(microphone, true, 10, audioSampleRate);
-        audioSource.loop = true;
-        Debug.Log(Microphone.IsRecording(microphone).ToString());
-
-        if (Microphone.IsRecording(microphone))
-        {
-            while (!(Microphone.GetPosition(microphone) > 0))
-            {
-                // Wait until recording starts
+                options.Add(device);
             }
 
-            Debug.Log("Started recording with " + microphone);
-            audioSource.Play();
+            Debug.Log(count + " input device(s) detected. Defaulting to first device.");
+            microphone = options[chosenDevice];
+            inputDeviceExists = true;
+            UpdateMicrophone();
         }
 
         else
         {
-            Debug.Log(microphone + " is not recording properly.");
+            Debug.Log("There is no input device detected.");
+            inputDeviceExists = false;
+        }
+    }
+
+    void UpdateMicrophone()
+    {
+        if (inputDeviceExists)
+        {
+            audioSource.Stop();
+            audioSource.clip = Microphone.Start(microphone, true, 10, AudioSettings.outputSampleRate);
+            audioSource.loop = true;
+            Debug.Log("Microphone is recording: " + Microphone.IsRecording(microphone).ToString());
+
+            if (Microphone.IsRecording(microphone))
+            {
+                while (!(Microphone.GetPosition(microphone) > 0))
+                {
+                    // Wait until recording starts
+                }
+
+                Debug.Log("Started recording with " + microphone);
+                audioSource.Play();
+            }
+
+            else
+            {
+                Debug.Log(microphone + " is not recording properly.");
+            }
         }
     }
 }
